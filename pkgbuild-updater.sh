3ffb843e07389ed4d2a4d076f080d9d87e0593f8
Jake Leahy <jake@leahy.dev>
2023-06-27 17:07:40 +1000

    Use the source URLs to determine where to find the latest version
    This fixes problems with PKGBUILDS that point to the softwares website
    
    Also rebuild .SRCINFO if the pkgrel changes

--- pkgbuild-updater.sh
+++ pkgbuild-updater.sh
@@ -5,19 +5,20 @@ BUILD_FILE=./PKGBUILD
 source "$BUILD_FILE"
 # Store old version so we know if we need to increment pkgrel
 oldPkgver=$pkgver
-
 echo "Updating: $pkgname"
-# Figure out how we will get the new version
-case "$url" in
+# Find the first source URL.
+# We will use that to try and find the latest version
+sourceURL=$(sed -n -E "s/^source.*=\(\".*::(.*)\".*\)/\1/p" "$BUILD_FILE" | head -n 1)
+case "$sourceURL" in
   https://github.com*)
     # I'm going to assume most projects will make tags.
     # Github seems to make tags in order of release so no need to sort the tags.
     # We do need to remove the 'v' prefix though
-    tagsUrl=$(echo "$url" | sed "s@https://github.com/\(.*\)@https://api.github.com/repos/\1/tags@")
+    tagsUrl=$(echo "$sourceURL" | sed -E "s@https://github.com/([^/]*)/([^/]*)/?.*@https://api.github.com/repos/\1/\2/tags@")
     pkgver=$(curl --silent "$tagsUrl" | jq -r .[0].name | sed "s/^v//")
   ;;
   # TODO: Give user option to specify version themsevles, maybe a --useVersion flag or something
-  *) 
+  *)
     echo "Unknown source. Please make an issue or PR to add this source"
     exit 1
   ;;
@@ -27,9 +28,8 @@ if [ -z "$pkgver" ]; then
   echo "Error when trying to get version" 1>&2
   exit 1
 fi
-
 # Update the version in the PKGBUILD
-sed -i "s/^pkgver=.*/pkgver=$pkgver/" $BUILD_FILE
+sed -i "s/^pkgver=.*/pkgver=$pkgver/" "$BUILD_FILE"
 # Now we need to update the hashes
 updpkgsums $BUILD_FILE
 # Rebuild .SRCINFO
@@ -42,9 +42,11 @@ elif [[ "$pkgver" != "$oldPkgver" ]]; then
   # We need to reset pkgrel to 1 if we incremented the version
   pkgrel=1
 fi
-sed -i "s/^pkgrel=.*/pkgrel=$pkgrel/" $BUILD_FILE
-  
 echo "New version: $pkgver-$pkgrel"
+sed -i "s/^pkgrel=.*/pkgrel=$pkgrel/" $BUILD_FILE
+# Rebuild .SRCINFO again incase the pkgrel number changed
+makepkg --printsrcinfo > .SRCINFO
+
 # And commit the changes (Should I ask the user if the changes are a-ok?)
 git commit .SRCINFO $BUILD_FILE -m "Bump to $pkgver-$pkgrel"
 
